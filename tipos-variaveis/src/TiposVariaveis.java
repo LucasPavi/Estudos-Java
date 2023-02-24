f19c433f8b3ca9253444b32e6c0407f98f682e32
Lucas Pavi <pavi_00@hotmail.com>
2023-02-23 22:56:51 -0300

    commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Lucas Pavi";
+
+        final double PI = 3.14F;
+
+        byte temperatura = 10;
+        long cpf = 10055821396L;
+        double salario = 2640;
+        short ano = 2023;
+        int rg = 2364125;
+
     }
 }
